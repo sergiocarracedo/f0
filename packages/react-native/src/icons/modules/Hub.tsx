ae6ae4f49fb44be1ebfad190ef1a0ad430ff514e
oscarbyte <36045526+oscarbyte@users.noreply.github.com>
2026-02-10 15:41:55 +0000

    fix(Hub): remove unsupported filter attribute from SVG

--- packages/react-native/src/icons/modules/Hub.tsx
+++ packages/react-native/src/icons/modules/Hub.tsx
@@ -10,7 +10,7 @@ const SvgHub = (props: SvgProps, ref: Ref<Svg>) => (
     ref={ref}
     {...props}
   >
-    <G fill="currentColor" filter="url(#Hub_svg__a)">
+    <G fill="currentColor">
       <Path d="M4.5 7.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0M4.5 15A1.5 1.5 0 0 1 6 13.5h3a1.5 1.5 0 0 1 1.5 1.5v3A1.5 1.5 0 0 1 9 19.5H6A1.5 1.5 0 0 1 4.5 18zM13.5 6A1.5 1.5 0 0 1 15 4.5h3A1.5 1.5 0 0 1 19.5 6v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 13.5 9zM13.5 15a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a1.5 1.5 0 0 1-1.5-1.5z" />
     </G>
     <Defs></Defs>

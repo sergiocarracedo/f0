011793a320ccce2bcb0f0d7cbdb15f5891715bce
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2026-02-18 17:06:22 +0100

    feat: include avatar list as a variant in DetailsItemsList component (#3470)

--- packages/react/src/experimental/Lists/DetailsItem/index.tsx
+++ packages/react/src/experimental/Lists/DetailsItem/index.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps, FC, forwardRef } from "react"
 
+import { F0AvatarList } from "@/components/avatars/F0AvatarList"
+import { F0AvatarListProps } from "@/components/avatars/F0AvatarList/types"
 import { Weekdays } from "@/experimental/Widgets/Content/Weekdays"
 import { experimentalComponent } from "@/lib/experimental"
 import { cn } from "@/lib/utils"
@@ -25,6 +27,10 @@ type Content =
   | (ComponentProps<typeof DataList.DotTagItem> & {
       type: "dot-tag"
     })
+  | {
+      type: "avatar-list"
+      avatarList: F0AvatarListProps
+    }
 
 export interface DetailsItemType {
   title: string
@@ -45,6 +51,11 @@ const ItemContent: FC<{ content: Content }> = ({ content }) => (
     {content.type === "team" && <DataList.TeamItem {...content} />}
     {content.type === "company" && <DataList.CompanyItem {...content} />}
     {content.type === "dot-tag" && <DataList.DotTagItem {...content} />}
+    {content.type === "avatar-list" && (
+      <li className="w-fit list-none px-1.5 py-1">
+        <F0AvatarList {...content.avatarList} />
+      </li>
+    )}
   </>
 )
 
